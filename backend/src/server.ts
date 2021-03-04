5d576fa64ac6e8c37760662a6fd673c0e2ddbfeb
Felipe Frantz Zanini <devffzanini@gmail.com>
2021-03-04 02:29:22 -0300

    :fire: burned documents

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/navers', (request, response) => {
-  console.log('hello')
-})
-
-app.listen(3333, () => { console.log('Server Runnning')})
